ae6fd8bd3b770ddf0b817ab28a7b0dcb56b68b32
rujanshakya <rujan.shakya123@gmail.com>
2022-09-27 11:08:36 +0545

    Added Models for ImportSource

---
+++ MiniProjectFile/Models/ImportSource.cs
@@ -0,0 +1,14 @@
+﻿namespace MiniProjectFile.Models
+{
+    public class ImportSource
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? FileName { get; set; }
+        public string? FilePath { get; set; }
+        public string? FileFormat { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
